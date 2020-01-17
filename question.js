4d612d608750894b36d1cbd7ef7a4318a9b01dbc
RisenFenix <jjstram@gmail.com>
2020-01-16 23:42:34 -0600

    somewhat working needs more time

--- question.js
+++ question.js
@@ -2,27 +2,27 @@ var questions = [
     {
         title: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        correctAnswer: "alerts"
     },
     {
         title: "The condition in an if / else statement is enclosed within ____.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        correctAnswer: "parentheses"
     },
     {
         title: 'What is the correct syntax for referring to an external script called "xxx.js"?',
         choices: ['<script name = "xxx.js">', '<script src = "xxx.js">', '<script href = "xxx.js">', '<script call = "xxx.js">'],
-        answer: '<script src = "xxx.js">',
+        correctAnswer: '<script src = "xxx.js">',
     },
     {
         title: 'How do you write "Hello World" in an alert box?',
         choices: ['msg("Hello World");', 'msgBox("Hello World");', 'alert("Hello World");', 'alertBox("Hello World");'],
-        answer: 'alert("Hello World");',
+        correctAnswer: 'alert("Hello World");',
     },
     {
         title: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
         choices: ["if(i <> 5)", "if i <> 5", "if i =! 5 then", "if (i != 5)"],
-        answer: "if (i != 5)",
+        correctAnswer: "if (i != 5)",
     },
     ///etc.
 ];
--- script.js
+++ script.js
@@ -1,24 +1,103 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Handler when the DOM is fully loaded
-    var showThatQuiz = document.querySelector(".startQuizButton");
-    var getQuestionTitle = document.querySelector("#questionTitle");
+    var showThatQuiz = document.querySelector(".startQuizBtn");
+    var getQuestionTitle = document.querySelector("#titleHeadings");
     var getQuestionChoices = document.querySelector("#questionChoices")
+    var timeWatch = document.querySelector(".activeTimer");
+
+    //var mainEl = document.getElementById("main");
+
+    var timerStartsAt = 75;
+    var index = 0;
+
+    showThatQuiz.addEventListener('click', function () {
 
 
-    for (var i = 0; i < questions.length; i++) {
-        var addedTitle = questions[i].title;
+        var startbutton = document.querySelector('.startQuizBtn');
+        startbutton.style.display = 'none';
+        startQuiz(index);
+        startTimer();
+
+
+    });
+
+    function startQuiz() {
+        // see first question
+        // seee first answer set
+        //  
+        showQuestion(index);
+        document.querySelectorAll(".currentChoices").addEventListener("click", function () {
+            checkAnswers();
+        });
+    }
+
+    function showQuestion(index) { //
+        // clear existing question set    
+
+        var thisQuestion = questions[index];
+        var addedTitle = thisQuestion.title;
         console.log(addedTitle);
-        getQuestionTitle.textContent = addedTitle;
+        var createQuestionTitles = document.createElement("h3");
+        createQuestionTitles.setAttribute("class", "currentQuestion");
+        createQuestionTitles.innerText = addedTitle;
+        createQuestionTitles.setAttribute
+        getQuestionTitle.appendChild(createQuestionTitles);
+        //console.log("current question is " + currentQuestion);
+        iterateAnswers(thisQuestion);
+    }
 
-        for (var fewChoices = 0; fewChoices < questions[i].choices.length; fewChoices++) {
-            var addedChoices = questions[i].choices[fewChoices];
+    function iterateAnswers(thisQuestion) {
+        for (var i = 0;
+            i < thisQuestion.choices.length;
+            i++) {
+
+            var addedChoices = thisQuestion.choices[i];
             console.log(addedChoices);
-            getQuestionChoices.textContent = addedChoices;
-        }
-    };
-    //postedQuestion function () {
+            // getQuestionChoices.textContent = addedChoices;
+            var createQuestionButtons = document.createElement("button");
+            createQuestionButtons.setAttribute("class", "currentChoices");
+            createQuestionButtons.setAttribute("value", addedChoices);
+            var breakbutton = document.createElement('br');
+            createQuestionButtons.textContent = addedChoices;
+            getQuestionChoices.appendChild(createQuestionButtons);
+            getQuestionChoices.appendChild(breakbutton);
 
-    //}
+
+        }
+    }
+
+
+    function checkAnswers() {
+        //if answer === correctAnswer nextquestion
+        var selectedAnswer = document.querySelector(".currentChoices").value;
+        if (selectedAnswer === questions[0].correctAnswer) {
+            document.getElementById(".currentQuestion").remove();
+            document.getElementById(".currentChoices").remove();
+            index++;
+            startQuiz(index);
+        } else {
+            timerStartsAt = timerStartsAt - 15;
+            document.getElementById(".currentQuestion").remove();
+            document.getElementById(".currentChoices").remove();
+            index++;
+            startQuiz(index);
+        }
+    }
+
+
+    function startTimer() {
+        var timerInterval = setInterval(function () {
+            timerStartsAt--;
+            timeWatch.textContent = timerStartsAt;
+
+            if (timerStartsAt === 0) {
+                clearInterval(timerInterval);
+                //insert stop function here
+                return;
+            }
+
+        }, 1000);
+    }
 
 
 
